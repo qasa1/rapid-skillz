700362c57e0ad4dfa1157d69a0d959a13b91c148
Ahsan Shahid <ahsanshahidcool@gmail.com>
2020-05-19 00:39:08 -0400

    Finished implementation of Djikstra as shortest path on skills graph

--- src/Components/graph.component.jsx
+++ src/Components/graph.component.jsx
@@ -1,10 +1,111 @@
 import React, { Component } from 'react'
+import {basicFrontEndSkills, basicFrontEndEdges} from '../Data/skills-store';
+import PriorityQueue from './priorityQ.component';
 
 export default class Graph extends Component {
+    constructor(props) {
+        super(props);
+        this.adjacencyList = {};
+        this.state = {
+            fastestPath: []
+        }
+    }
+
+    componentDidMount() {
+        this.buildGraph();
+        this.addEdges();
+        let tits = this.djikstra(1,25);
+
+        console.log(tits);
+    }
+
+    buildGraph = () => {
+        for (let node of basicFrontEndSkills) {
+            this.addVertex(node.name);
+        }
+    }
+
+    addVertex = (value) => {
+        if (!this.adjacencyList[value]) {
+            this.adjacencyList[value] = []
+        }
+    }
+
+    addEdges = () => {
+        for (let edge of basicFrontEndEdges) {
+            let v1 = basicFrontEndSkills.filter((obj) =>  obj.id === edge[0])[0];
+            let v2 = basicFrontEndSkills.filter((obj) =>  obj.id === edge[1])[0];
+            this.adjacencyList[v1.name].push({node: v2.name , weight: v1.time + v2.time});
+        }
+    }
+
+    djikstra(startID, finishID) {
+        const nodes = new PriorityQueue()
+        let distances = {}
+        let previous = {}
+        let result = []
+
+        // Fetch start and finish nodes
+        let start = basicFrontEndSkills.filter((obj) => obj.id === startID)[0];
+        let finish = basicFrontEndSkills.filter((obj) => obj.id === finishID)[0];
+
+        // Build up initial state
+        for (let node in this.adjacencyList) {
+        if (node === start.name) {
+            distances[node] = 0
+            nodes.enqueue(node, distances[node])
+        } else {
+            distances[node] = Infinity
+            nodes.enqueue(node, distances[node])
+        }
+        previous[node] = null
+        }
+
+
+        // The main loop - while priority queue is populated
+
+        while (nodes.values.length) {
+        let currentNode = nodes.dequeue()
+        if (currentNode.val === finish.name) {
+            // We are done
+            let item = previous[currentNode.val]
+            while (item) {
+            // Update state
+            result.unshift(item);
+
+            item = previous[item]
+            }
+
+            result.push(finish.name);
+            this.setState({
+                fastestPath: result
+            })
+
+            break;
+        }
+
+        if (currentNode.val || distances[currentNode] !== Infinity) {
+            // Loop through neighbors of current node
+            for (let neighbor of this.adjacencyList[currentNode.val]) {
+                console.log("currentNode", currentNode, "neighbnor", neighbor )
+            let candidate = currentNode.distance + neighbor.weight;
+            if (candidate < distances[neighbor.node]) {
+                distances[neighbor.node] = candidate
+                previous[neighbor.node] = currentNode.val
+                nodes.enqueue(neighbor.node, candidate)
+            }
+            }
+
+            console.log("previous", previous);
+        }
+        }
+       
+    }
+
     render() {
         return (
             <div>
-                
+                {this.state.fastestPath.map((item, index) => <p key={index}>{item}</p>)}
             </div>
         )
     }
---
+++ src/Data/skills-store.js
@@ -0,0 +1,178 @@
+const basicFrontEndSkills = [
+    {
+        id: 1,
+        name: "HTML5 Basics (Semantic Elements, Attributes)",
+        time: 10
+    },
+    {
+        id: 2,
+        name: "CSS Fundamentals",
+        time: 20
+    },
+    {
+        id: 3,
+        name: "CSS Grid and Flexbox",
+        time: 5
+    },
+    {
+        id: 4,
+        name: "CSS Custom Properties",
+        time: 5
+    },
+    {
+        id: 5,
+        name: "CSS Transitions",
+        time: 2
+    },
+    {
+        id: 6,
+        name: "Responsive Design",
+        time: 10
+    },
+    {
+        id: 7,
+        name: "SASS",
+        time: 4
+    },
+    {
+        id: 8,
+        name: "LESS",
+        time: 7
+    },
+    {
+        id: 9,
+        name: "Stylus",
+        time: 6
+    },
+    {
+        id: 10,
+        name: "PostCSS",
+        time: 10
+    },
+    {
+        id: 11,
+        name: "Bootstrap",
+        time: 20
+    },
+    {
+        id: 12,
+        name: "Tailwind CSS",
+        time: 10
+    },
+    {
+        id: 13,
+        name: "Materialize",
+        time: 10
+    },
+    {
+        id: 14,
+        name: "Bulma",
+        time: 15
+    },
+    {
+        id: 15,
+        name: "Vanilla Javascript",
+        time: 30
+    },
+    {
+        id: 16,
+        name: "Git (Version Control)",
+        time: 5
+    },
+    {
+        id: 17,
+        name: "Browser Dev Tools",
+        time: 1
+    },
+    {
+        id: 18,
+        name: "NPM",
+        time: 1
+    },
+    {
+        id: 19,
+        name: "Yarn",
+        time: 1
+    },
+    {
+        id: 20,
+        name: "Webpack",
+        time: 3
+    },
+    {
+        id: 21,
+        name: "Parcel",
+        time: 2
+    },
+    {
+        id: 22,
+        name: "Domain Registration",
+        time: 3
+    },
+    {
+        id: 23,
+        name: "Netlify",
+        time: 2
+    },
+    {
+        id: 24,
+        name: "Github Pages",
+        time: 0.5
+    },
+    {
+        id: 25,
+        name: "Basic Front-end Developer",
+        time: 0
+    }
+]
+
+const basicFrontEndEdges = [
+   [1,2],
+   [2,3],
+   [3,4],
+   [4,5],
+   [5,6],
+   [6,7],
+   [6,8],
+   [6,9],
+   [6,10],
+   [7,11],
+   [7,12],
+   [7,13],
+   [7,14],
+   [8,11],
+   [8,12],
+   [8,13],
+   [8,14],
+   [9,11],
+   [9,12],
+   [9,13],
+   [9,14],
+   [10,11],
+   [10,12],
+   [10,13],
+   [10,14],
+   [11,15],
+   [12,15],
+   [13,15],
+   [14,15],
+   [15,16],
+   [16,17],
+   [17,18],
+   [17,19],
+   [18,20],
+   [18,21],
+   [19,20],
+   [19,21],
+   [20,22],
+   [21,22],
+   [22,23],
+   [22,24],
+   [23,25],
+   [24,25] 
+]
+
+export {
+    basicFrontEndSkills,
+    basicFrontEndEdges
+}
